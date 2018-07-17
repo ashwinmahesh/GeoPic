04b25352db245ad7f03af3cc07b83902bc228ee5
Ashwin <mahesh.ashwin1998@gmail.com>
2018-07-17 15:20:54 -0700

    Explore storyboard UI complete

---
+++ GeoPic/ExploreVC.swift
@@ -0,0 +1,35 @@
+//
+//  ExploreVC.swift
+//  GeoPic
+//
+//  Created by Ashwin Mahesh on 7/17/18.
+//  Copyright © 2018 AshwinMahesh. All rights reserved.
+//
+
+import UIKit
+
+class ExploreVC: UIViewController {
+    var tableData:[String]=[]
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate=self
+        tableView.dataSource=self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+extension ExploreVC:UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PictureCell", for: indexPath) as! PictureCell
+        
+        return cell
+    }
+}
---
+++ GeoPic/PictureCell.swift
@@ -0,0 +1,28 @@
+//
+//  PictureCell.swift
+//  GeoPic
+//
+//  Created by Ashwin Mahesh on 7/17/18.
+//  Copyright © 2018 AshwinMahesh. All rights reserved.
+//
+
+import UIKit
+
+class PictureCell: UITableViewCell {
+
+    @IBOutlet weak var descriptionView: UITextView!
+    @IBOutlet weak var pictureView: UIImageView!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
